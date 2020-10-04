a837d56fc80c41222def30dc43f0094b5fdd0aa1
StanPeter <stanislav.petr7@gmail.com>
2020-10-05 01:04:36 +0200

    adding CRUD functionality

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_LEADS } from './types';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
 
 //GET LEADS
 export const getLeads = () => dispatch => {
@@ -12,3 +12,27 @@ export const getLeads = () => dispatch => {
         })
         .catch(e => prompt(e, 'ERROR'));
 };
+
+//DELETE LEAD
+export const deleteLead = (id) => dispatch => {
+    axios.delete(`/api/leads/${id}`)
+        .then(res => {
+            dispatch({
+                type: DELETE_LEAD,
+                payload: id
+            });
+        })
+        .catch(err => console.log(err));
+}
+
+//ADD LEAD
+export const addLead = (lead) => dispatch => {
+    axios.post('/api/leads/', lead)
+        .then(res => {
+            dispatch({
+                type: ADD_LEAD,
+                payload: res.data
+            });
+        })
+        .catch(err => console.log(err));
+}
--- leadmanager/frontend/src/components/Leads/Leads.js
+++ leadmanager/frontend/src/components/Leads/Leads.js
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getLeads } from '../../actions/leads';
+import { getLeads, deleteLead } from '../../actions/leads';
 
 export class Leads extends Component {
     static propTypes = {
-        leads: PropTypes.array.isRequired
+        leads: PropTypes.array.isRequired,
+        getLeads: PropTypes.func.isRequired,
+        deleteLead: PropTypes.func.isRequired
     }
 
     componentDidMount() {
@@ -32,7 +34,11 @@ export class Leads extends Component {
                                 <td>{lead.name}</td>
                                 <td>{lead.email}</td>
                                 <td>{lead.message}</td>
-                                <td><button className="btn btn-danger btn-sm">Delete</button></td>
+                                <td>
+                                    <button
+                                        onClick={this.props.deleteLead.bind(this, lead.id)}
+                                        className="btn btn-danger btn-sm">Delete</button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
@@ -46,4 +52,4 @@ const mapStateToProps = state => ({
     leads: state.leadReducer.leads
 });
 
-export default connect(mapStateToProps, { getLeads })(Leads);
+export default connect(mapStateToProps, { getLeads, deleteLead })(Leads);
