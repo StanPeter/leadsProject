75df462ccf5768e0e706ecdd8e5e50f886dabdea
StanPeter <stanislav.petr7@gmail.com>
2020-10-11 17:39:59 +0200

    adding authentication api

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { createMessage } from './messages'
+import { createMessage, returnErrors } from './messages'
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
 
 //GET LEADS
@@ -11,7 +11,7 @@ export const getLeads = () => dispatch => {
                 payload: res.data
             })
         })
-        .catch(e => prompt(e, 'ERROR'));
+        .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
 
 //DELETE LEAD
@@ -39,14 +39,5 @@ export const addLead = (lead) => dispatch => {
                 payload: res.data
             });
         })
-        .catch(err => {
-            const errors = {
-                msg: err.response.data,
-                status: err.response.status
-            };
-            dispatch({
-                type: GET_ERRORS,
-                payload: errors
-            });
-        });
+        .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Lead(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True) #no duplicates
+    email = models.EmailField(max_length=100, unique=True)  # no duplicates
     message = models.CharField(max_length=500, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True) #add now date
+    created_at = models.DateTimeField(auto_now_add=True)  # add now date
+    owner = models.ForeignKey(
+        User, related_name='leads', on_delete=models.CASCADE, null=True)
